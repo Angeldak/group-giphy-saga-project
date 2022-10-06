f7b2db1a55b6a66f7bc448c09860f3e7049a4093
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-06 17:01:03 -0500

    setup categories at li

--- src/components/FavListItem/CategoryListItem/CategoryListItem.jsx
+++ src/components/FavListItem/CategoryListItem/CategoryListItem.jsx
@@ -1,9 +1,9 @@
 function CategoryListItem({ item }) {
   return (
-    <>
+    <li>
       <input value={item.name} type="radio" name="categories" id={item.name} />
       <label htmlFor={item.name}>{item.name}</label>
-    </>
+    </li>
   );
 }
 
