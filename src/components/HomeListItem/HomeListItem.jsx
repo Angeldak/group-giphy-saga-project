4bd56d3e2c07a32070cc34b14c5d58efc2665b92
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-07 15:00:19 -0500

    Fixing sizing

--- src/components/HomeListItem/HomeListItem.jsx
+++ src/components/HomeListItem/HomeListItem.jsx
@@ -1,26 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
-import './HomeListItem.css'
+import "./HomeListItem.css";
 
 function HomeListItem(props) {
-    const favorites = useSelector(store => store.favorites);
-    const dispatch = useDispatch();
+  const favorites = useSelector((store) => store.favorites);
+  const dispatch = useDispatch();
 
-    const markFavorite = (url) => {
-            dispatch({
-            type: 'ADD_FAVORITE',
-            payload: url
-        });
-    }
-    console.log('This is props.fav', props.alreadyFavorite);
-    return(
-        <div  key={props.index} className="resultGifDiv">
-            <img src={props.gif.images.fixed_height.url} />
-            <div>
-                {props.alreadyFavorite?<p className="searchGifButtons">💚</p>:<button className="favButton" onClick={() => markFavorite(props.gif.images.fixed_height.url)}>Add to Favorites 💚</button>}
-                <h4 className="searchGifButtons">{props.gif.title}</h4>
-            </div>
-        </div>
-    )
+  const markFavorite = (url) => {
+    dispatch({
+      type: "ADD_FAVORITE",
+      payload: url,
+    });
+  };
+  console.log("This is props.fav", props.alreadyFavorite);
+  return (
+    <div key={props.index} className="resultGifDiv">
+      <img src={props.gif.images.fixed_height.url} />
+      <div>
+        {props.alreadyFavorite ? (
+          <p className="searchGifButtons">💚</p>
+        ) : (
+          <button
+            className="favButton"
+            onClick={() => markFavorite(props.gif.images.original.url)}
+          >
+            Add to Favorites 💚
+          </button>
+        )}
+        <h4 className="searchGifButtons">{props.gif.title}</h4>
+      </div>
+    </div>
+  );
 }
 
 export default HomeListItem;
