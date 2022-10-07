7772fad8285237769891c8c96ed81655436aeed0
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-07 15:01:44 -0500

    fixed size check

--- src/components/HomeList/HomeList.jsx
+++ src/components/HomeList/HomeList.jsx
@@ -1,37 +1,43 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import HomeListItem from "../HomeListItem/HomeListItem";
-import "./HomeList.css"
+import "./HomeList.css";
 
 function HomeList() {
-    const searchResults = useSelector(store => store.gifReducer);
-    const favorites = useSelector(store => store.favorites);
-    const dispatch = useDispatch();
+  const searchResults = useSelector((store) => store.gifReducer);
+  const favorites = useSelector((store) => store.favorites);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch({
-            type: 'GET_FAVORITES'
-        })
-    }, []);
+  useEffect(() => {
+    dispatch({
+      type: "GET_FAVORITES",
+    });
+  }, []);
 
-    return (
-        <div className="searchResultsDiv container">
-            <div className="searchHeading">
-            <h2>Search Results</h2>
-            </div>
-            <div className="searchGrid">
-            {'data' in searchResults && searchResults.data.map((gif, index) => {
-                    let urlCheck = favorites.some((item) => {
-                        return String(item.url) === String(gif.images.fixed_height.url)
-                    }) 
-                     return (
-                        <HomeListItem key={index} gif={gif} index={index} favorites={favorites} alreadyFavorite={urlCheck}/>
-                            )
-            })}
-            </div>
-        </div>
-        
-    )
+  return (
+    <div className="searchResultsDiv container">
+      <div className="searchHeading">
+        <h2>Search Results</h2>
+      </div>
+      <div className="searchGrid">
+        {"data" in searchResults &&
+          searchResults.data.map((gif, index) => {
+            let urlCheck = favorites.some((item) => {
+              return String(item.url) === String(gif.images.original.url);
+            });
+            return (
+              <HomeListItem
+                key={index}
+                gif={gif}
+                index={index}
+                favorites={favorites}
+                alreadyFavorite={urlCheck}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default HomeList;
