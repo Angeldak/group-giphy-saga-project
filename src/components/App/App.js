a548cdd87dd8fc553617bcfbf0c751cce0715f39
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-07 14:08:53 -0500

    header and navlink setup along with container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,36 @@ import React from "react";
 import Header from "../Header/Header";
 import Home from "../Pages/Home/Home";
 import Favorites from "../Pages/Favorites/Favorites";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 import Categories from "../Pages/Categories/Categories";
+import "./App.css";
 
 function App(props) {
   return (
     <div className="App">
       <Header />
       <Router>
+        <div className="link-div">
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "link-name")}
+            to="/"
+            exact
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "link-name")}
+            to="/favorites"
+          >
+            Favorites
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "link-name")}
+            to="/categories"
+          >
+            Categories
+          </NavLink>
+        </div>
         <Route path="/" exact>
           <Home />
         </Route>
